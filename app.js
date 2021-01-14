424ecfb2d96bb831b3c0e6607c9efa0c7ec15495
Jay Desai <jaydesai571@gmail.com>
2021-01-14 00:20:56 -0500

    Database added with one cluster

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
+//Middlewares
+
+
+//ROUTES
+app.get('/', (req,res) => {
+    res.send('We are on home');
+});
+
+app.get('/posts', (req,res) => {
+    res.send('We are on post');
+});
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECTION,
+    {useNewUrlParser: true},
+    () => {console.log("Database connected");
+});
+app.listen(3000);
