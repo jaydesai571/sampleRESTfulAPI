041bf282340e9e3d3b7d7bdb46c05bac1010549c
Jay Desai <jaydesai571@gmail.com>
2021-01-14 19:46:40 -0500

    Basic route testing and samples developed

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv/config');
-//Middlewares
 
+//Middlewares
+const postRoute = require('./routes/posts');
+app.use('/posts', postRoute);
 
 //ROUTES
 app.get('/', (req,res) => {
     res.send('We are on home');
 });
 
-app.get('/posts', (req,res) => {
-    res.send('We are on post');
+app.get('/post', (req,res) => {
+    res.send('We are on the post');
 });
 
 //Connect to DB
